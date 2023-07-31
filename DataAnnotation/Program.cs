843c0f9f380096887fee634dc7c46a15bf860285
Upendra Sahu <upendrasahu1199@gmail.com>
2023-07-31 23:11:32 +0530

    Data Annotation Program

---
+++ DataAnnotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotation
+{
+    class Person
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Range(10, 100, ErrorMessage = "Age Must be between 18 and 100.")]
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 12;
+
+            Console.WriteLine(person.Age);
+            Console.ReadLine();
+        }   
+    }
+}
